fff79528926d77a0f969376490afa2aa429882c3
Saikrishna41 <saai.krrishna@gmail.com>
2023-12-07 15:41:31 +0800

    row with buttons

--- app/src/main/java/com/techfortyone/jetpackcomposecodetutor/MainActivity.kt
+++ app/src/main/java/com/techfortyone/jetpackcomposecodetutor/MainActivity.kt
@@ -3,11 +3,16 @@ package com.techfortyone.jetpackcomposecodetutor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techfortyone.jetpackcomposecodetutor.ui.theme.JetpackComposeCodetutorTheme
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +35,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Click me")
+        }
+
+        Text(text = "0")
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Click me")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeCodetutorTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
